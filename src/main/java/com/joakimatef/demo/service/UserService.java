4073020e2a96101732842890b9bdb0ce88d124d0
Joakim Briceno <joakim.onnhage@gmail.com>
2021-02-11 18:33:05 +0100

    Added so you can't create a user that already exists

--- src/main/java/com/joakimatef/demo/service/UserService.java
+++ src/main/java/com/joakimatef/demo/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,6 +44,9 @@ public class UserService {
 
     public User createAdmin(User user) {
         Role adminRole = roleRepository.findByRoleName("ADMIN").orElseThrow(() -> new RuntimeException("Role not found"));
+        Optional<User> alreadyExistingUser = userRepository.findByUsername(user.getUsername());
+        if(alreadyExistingUser.isPresent())
+            throw new RuntimeException(String.format("User with %s already exists", alreadyExistingUser.get().getUsername()));
 
         return userRepository.save(User.builder()
                 .username(user.getUsername())
