59a5e3a93f7b526eab881d96cf83ed11f23db732
Joakim Briceno <joakim.onnhage@gmail.com>
2021-02-15 17:47:08 +0100

    Added more javadocs

--- src/main/java/com/joakimatef/demo/config/SecurityConfig.java
+++ src/main/java/com/joakimatef/demo/config/SecurityConfig.java
@@ -18,26 +18,45 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ *  Class used to configure Spring security
+ * @author  Joakim Önnhage
+ * @version 1.0
+ * @since   2021-02-15
+ */
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    private final JpaUserDetailService myUserDetailsService;
+    /**
+     * Used for configuring {@link AuthenticationManager}
+     */
+    private final JpaUserDetailService jpaUserDetailService;
+    /**
+     * Used for configuring {@link HttpSecurity} to add filter for JWT
+     */
     private final JwtRequestFilter jwtRequestFilter;
 
     @Autowired
-    public SecurityConfig(JpaUserDetailService myUserDetailsService, JwtRequestFilter jwtRequestFilter){
-        this.myUserDetailsService = myUserDetailsService;
+    public SecurityConfig(JpaUserDetailService jpaUserDetailService, JwtRequestFilter jwtRequestFilter){
+        this.jpaUserDetailService = jpaUserDetailService;
         this.jwtRequestFilter = jwtRequestFilter;
     }
 
+    /**
+     * Configuring the authentication manager with {@link JpaUserDetailService}
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+        auth.userDetailsService(jpaUserDetailService);
     }
 
+    /**
+     * Security method configuring the requests done form http
+     * Filter added for JWT
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize ->
